f91d4bf490c264e95975b0345f02a77a3968c7fa
mmaakkyyii <kyoro3141592@gmail.com>
2020-09-13 03:38:26 +0900

    po

--- word_generator.js
+++ word_generator.js
@@ -1,6 +1,7 @@
 var word_list=["もぁぱち","空想部","ゆきたん","ひよたん","空想","魂","無限","あんどもあ","空想力","ぱちぱち","羊","クーピー","アルパカ","ヌートリア","ナマコ","クレオン","屋上","輪廻","深淵","舞踏会","キセキ","晩餐","しゅき","わくわく","人生","ねーね","マジック","ゴーン","ナイト"];
 var word_list2=["の","の","と","",""];
 
+/*
 function getCSV(){
     let req=new XMLHttpRequest();
     req.open("get","DB/pachipachi_words.csv",true);
@@ -10,9 +11,9 @@ function getCSV(){
         console.log(req.responseText);
     }
 }
-
+*/
 function GenerateWord(){
-    getCSV();
+    //getCSV();
     let i1=Math.floor(Math.random()*word_list.length);
     let i2=Math.floor(Math.random()*word_list.length);
     let i3=Math.floor(Math.random()*word_list2.length);
