2220d2f0634a6900cdf13dda4fbe100acf5a22fe
mmaakkyyii <kyoro3141592@gmail.com>
2020-09-15 03:07:26 +0900

    データベース読み込み

---
+++ data_access.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const {Client} = require('pg');
+
+exports.GetDB= function (callback){
+  const client= new Client({
+    //pachipachi-word-generator
+    host: process.env.ENV_HOST,
+    database: process.env.ENV_DB,
+    port: 5432,
+    user: process.env.ENV_USER,
+    password: process.env.ENV_PASSWORD,
+    ssl: { rejectUnauthorized: false }
+  });
+  client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack);
+    } else {
+      console.log('connected');
+    }
+  })
+
+  let quer="SELECT * FROM words;";
+  client.query(quer, (err, res) => {
+    if (err) throw err;
+     for (let row of res.rows) {
+       //console.log(JSON.stringify(row));
+     }
+     client.end();
+     callback(res);
+   });
+
+  //console.log(query);
+  //return query;
+
+}
+
+exports.PutWord2DB = function(text){
+  const client= new Client({
+    //pachipachi-word-generator
+    host: process.env.ENV_HOST,
+    database: process.env.ENV_DB,
+    port: 5432,
+    user: process.env.ENV_USER,
+    password: process.env.ENV_PASSWORD,
+    ssl: { rejectUnauthorized: false }
+  });
+  client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+
+  
+  let quer="INSERT INTO words(word) VALUES('"+text+"');";
+  client.query(quer, (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
+
+}
--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const fs =require('fs');
+const DB =require('./data_access.js');
+let aa=[]
+let a=function(i){make_DB_json(i)};
+//DB.PutWord2DB("もぁぱちちゃん");
+//DB.GetDB(a);
+console.log(aa);
+
 const app = express();
 // パス指定用モジュール
 const path = require('path');
+const { table } = require('console');
 
 
 // 8080番ポートで待ちうける
 app.listen(process.env.PORT ||8080, () => {
   console.log('Running at Port 8080...');
+});
+
+app.get('/',(req,res)=>{
+  console.log('accsess!');
+  DB.GetDB(a);
+
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // 静的ファイルのルーティング
@@ -16,3 +32,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res) => {
   res.sendStatus(404);
 });
+
+
+function make_DB_json(i){
+  console.log(i.rows);
+  let data={
+    words:[]
+  };
+  for (let row of i.rows) {
+    console.log(row);
+    data.words.push(row);
+  }
+  fs.writeFile('./public/DB.json',JSON.stringify(data,'',' '),(err)=>{if(err)console.log('${err}')});
+
+}
